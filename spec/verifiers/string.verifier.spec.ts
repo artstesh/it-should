b26a3d25e00e6132225aa3069c126acb70a32ec4
artstesh <artstesh@mail.ru>
2023-02-07 18:08:24 +0300

    wip

--- spec/verifiers/string.verifier.spec.ts
+++ spec/verifiers/string.verifier.spec.ts
@@ -75,4 +75,161 @@ describe('StringVerifier', () => {
       expect(() => new StringVerifier(entry).notEmpty()).not.toThrow();
     })
   })
+
+  describe('beWhiteSpace', () => {
+
+    it('null throws', () =>{
+      expect(() => new StringVerifier(null).beWhiteSpace()).toThrow(ShouldError);
+    })
+    it('undefined throws', () =>{
+      expect(() => new StringVerifier(undefined).beWhiteSpace()).toThrow(ShouldError);
+    })
+    it('not empty throws', () =>{
+      const entry = Forger.create<string>();
+      //
+      expect(() => new StringVerifier(entry).beWhiteSpace()).toThrow(ShouldError);
+    })
+    it('single whitespace success', () =>{
+      expect(() => new StringVerifier(' ').beWhiteSpace()).not.toThrow();
+    })
+    it('a few whitespaces success', () =>{
+      expect(() => new StringVerifier('   ').beWhiteSpace()).not.toThrow();
+    })
+  })
+
+  describe('notWhiteSpace', () => {
+
+    it('null success', () =>{
+      expect(() => new StringVerifier(null).notWhiteSpace()).not.toThrow();
+    })
+    it('undefined success', () =>{
+      expect(() => new StringVerifier(undefined).notWhiteSpace()).not.toThrow();
+    })
+    it('not empty success', () =>{
+      const entry = Forger.create<string>();
+      //
+      expect(() => new StringVerifier(entry).notWhiteSpace()).not.toThrow();
+    })
+    it('single whitespace throws', () =>{
+      expect(() => new StringVerifier(' ').notWhiteSpace()).toThrow(ShouldError);
+    })
+    it('a few whitespaces throws', () =>{
+      expect(() => new StringVerifier('   ').notWhiteSpace()).toThrow(ShouldError);
+    })
+  })
+
+  describe('beUpperCased', () => {
+
+    it('null success', () =>{
+      expect(() => new StringVerifier(null).beUpperCased()).not.toThrow();
+    })
+    it('undefined success', () =>{
+      expect(() => new StringVerifier(undefined).beUpperCased()).not.toThrow();
+    })
+    it('whitespace success', () =>{
+      expect(() => new StringVerifier(' ').beUpperCased()).not.toThrow();
+    })
+    it('not uppercased throws', () =>{
+      const entry = Forger.create<string>({stringUpCase: false, stringLength: 50});
+      //
+      expect(() => new StringVerifier(entry).beUpperCased()).toThrow(ShouldError);
+    })
+    it('uppercased success', () =>{
+      const entry = Forger.create<string>({stringLowCase: false, stringLength: 50});
+      //
+      expect(() => new StringVerifier(entry).beUpperCased()).not.toThrow();
+    })
+  })
+
+  describe('notBeUpperCased', () => {
+
+    it('null success', () =>{
+      expect(() => new StringVerifier(null).notBeUpperCased()).not.toThrow();
+    })
+    it('undefined success', () =>{
+      expect(() => new StringVerifier(undefined).notBeUpperCased()).not.toThrow();
+    })
+    it('whitespace success', () =>{
+      expect(() => new StringVerifier(' ').notBeUpperCased()).not.toThrow();
+    })
+    it('uppercased success', () =>{
+      const entry = Forger.create<string>({stringLowCase: false, stringLength: 50});
+      //
+      expect(() => new StringVerifier(entry).notBeUpperCased()).toThrow(ShouldError);
+    })
+    it('not uppercased throws', () =>{
+      const entry = Forger.create<string>({stringUpCase: false, stringLength: 50});
+      //
+      expect(() => new StringVerifier(entry).notBeUpperCased()).not.toThrow();
+    })
+  })
+
+  describe('notBeLowerCased', () => {
+
+    it('null success', () =>{
+      expect(() => new StringVerifier(null).notBeLowerCased()).not.toThrow();
+    })
+    it('undefined success', () =>{
+      expect(() => new StringVerifier(undefined).notBeLowerCased()).not.toThrow();
+    })
+    it('whitespace success', () =>{
+      expect(() => new StringVerifier(' ').notBeLowerCased()).not.toThrow();
+    })
+    it('not uppercased throws', () =>{
+      const entry = Forger.create<string>({stringUpCase: false, stringLength: 50});
+      //
+      expect(() => new StringVerifier(entry).notBeLowerCased()).toThrow(ShouldError);
+    })
+    it('uppercased success', () =>{
+      const entry = Forger.create<string>({stringLowCase: false, stringLength: 50});
+      //
+      expect(() => new StringVerifier(entry).notBeLowerCased()).not.toThrow();
+    })
+  })
+
+  describe('beLowerCased', () => {
+
+    it('null success', () =>{
+      expect(() => new StringVerifier(null).beLowerCased()).not.toThrow();
+    })
+    it('undefined success', () =>{
+      expect(() => new StringVerifier(undefined).beLowerCased()).not.toThrow();
+    })
+    it('whitespace success', () =>{
+      expect(() => new StringVerifier(' ').beLowerCased()).not.toThrow();
+    })
+    it('uppercased success', () =>{
+      const entry = Forger.create<string>({stringLowCase: false, stringLength: 50});
+      //
+      expect(() => new StringVerifier(entry).beLowerCased()).toThrow(ShouldError);
+    })
+    it('not uppercased throws', () =>{
+      const entry = Forger.create<string>({stringUpCase: false, stringLength: 50});
+      //
+      expect(() => new StringVerifier(entry).beLowerCased()).not.toThrow();
+    })
+  })
+
+  describe('hasLength', () => {
+    let strLength: number;
+    let entry: string;
+
+    beforeEach(() => {
+      strLength = Forger.create<number>({numberMin: 5, numberMax: 20})!;
+      entry = Forger.create<string>({stringLength: strLength})!;
+    })
+
+    it('greater throws', () =>{
+      expect(() => new StringVerifier(entry).hasLength(strLength - 1)).toThrow(ShouldError);
+    })
+    it('smaller throws', () =>{
+      expect(() => new StringVerifier(entry).hasLength(strLength + 1)).toThrow(ShouldError);
+    })
+    it('undefined throws', () =>{
+      expect(() => new StringVerifier(undefined).hasLength(strLength)).toThrow(ShouldError);
+    })
+    it('equal success', () =>{
+      expect(() => new StringVerifier(entry).hasLength(strLength)).not.toThrow();
+    })
+  })
 })
--- src/verifiers/string.verifier.ts
+++ src/verifiers/string.verifier.ts
@@ -5,23 +5,58 @@ export class StringVerifier {
   constructor(private entry: string | null | undefined) {
   }
 
-  notEmpty(): void {
-    if (this.entry !== "") return;
+  notEmpty(): StringVerifier {
+    if (this.entry !== "") return this;
     throw new ShouldError(`${this.entry} is empty.`);
   }
 
-  notNullOrEmpty(): void {
-    if (!this.entry)
-      throw new ShouldError(`${this.entry} is empty or null.`);
+  notNullOrEmpty(): StringVerifier {
+    if (!!this.entry) return this;
+    throw new ShouldError(`${this.entry} is empty or null.`);
   }
 
-  beEmpty(): void {
-    if (this.entry === '') return;
+  beEmpty(): StringVerifier {
+    if (this.entry === "") return this;
     throw new ShouldError(`${this.entry} is not empty.`);
   }
 
-  beNullOrEmpty(): void {
-    if (!this.entry) return;
+  beNullOrEmpty(): StringVerifier {
+    if (!this.entry) return this;
     throw new ShouldError(`${this.entry} is not empty or null.`);
+  }
+
+  beWhiteSpace(): StringVerifier {
+    if (this.entry?.replaceAll(' ', '')?.length === 0) return this;
+    throw new ShouldError(`${this.entry} is not whitespace.`);
+  }
+
+  notWhiteSpace(): StringVerifier {
+    if (!this.entry || this.entry.replaceAll(' ', '').length !== 0) return this;
+    throw new ShouldError(`${this.entry} is whitespace.`);
+  }
+
+  beUpperCased(): StringVerifier {
+    if (!this.entry || this.entry === this.entry?.toUpperCase()) return this;
+    throw new ShouldError(`${this.entry} is not uppercased.`);
+  }
+
+  notBeUpperCased(): StringVerifier {
+    if (!this.entry || this.entry === this.entry?.toLowerCase()) return this;
+    throw new ShouldError(`${this.entry} is uppercased.`);
+  }
+
+  beLowerCased(): StringVerifier {
+    if (!this.entry || this.entry === this.entry?.toLowerCase()) return this;
+    throw new ShouldError(`${this.entry} is not uppercased.`);
+  }
+
+  notBeLowerCased(): StringVerifier {
+    if (!this.entry || this.entry === this.entry?.toUpperCase()) return this;
+    throw new ShouldError(`${this.entry} is uppercased.`);
+  }
+
+  hasLength(expected: number): StringVerifier {
+    if (this.entry?.length === expected) return this;
+    throw new ShouldError(`${this.entry} doesn't equal expected length ${expected}.`);
   }
 }
